b6de009dba924fe4d1acb7c44855904a24a53861
Sasha F <sfriedenberg@etsy.com>
2026-04-23 15:34:06 -0400

    chore: release v0.2.2

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
         packages = {
           default = pkgs.buildGoApplication {
             pname = "tommy";
-            version = "0.2.1";
+            version = "0.2.2";
             commit = self.rev or self.shortRev or "unknown";
             src = pkgs.lib.cleanSourceWith {
               src = ./.;
