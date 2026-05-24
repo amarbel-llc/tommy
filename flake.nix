8bdbf0eaeb85114149633b40aa9cfaf2610669ea
friedenberg <1baggzpr@starbrandshoes.com>
2026-05-24 13:57:21 -0400

    chore: release v0.2.8

--- flake.nix
+++ flake.nix
@@ -73,7 +73,7 @@
 
         tommyBin = pkgs.buildGoApplication {
           pname = "tommy";
-          version = "0.2.7";
+          version = "0.2.8";
           commit = self.rev or self.shortRev or "unknown";
           src = go-pkgs-test;
           modules = ./gomod2nix.toml;
