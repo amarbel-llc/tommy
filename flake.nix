ebd86ff30999352678c02d6baf25588564b41a02
friedenberg <1baggzpr@starbrandshoes.com>
2026-09-07 01:59:32 +0000

    bump nixpkgs-master: 567a49d → f13ff45
    conformist: 639a495 → f2660cb
    igloo: 2c8ca73 → b13d154
    nixpkgs-master: 567a49d → f13ff45

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     igloo.url = "https://code.linenisgreat.com/igloo/archive/master.tar.gz";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/567a49d1913ce81ac6e9582e3553dd90a955875f";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/f13ff45afd1bb73e640eaa08a7066dbed07e3238";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
     bats = {
       url = "https://code.linenisgreat.com/bats/archive/master.tar.gz";
